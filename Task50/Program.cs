104f928b794b5642b8a94b199a814733063bea85
Elena <el.kalenova@mail.ru>
2022-12-23 22:42:11 +0300

    Седьмое домашнее задание

---
+++ Task50/Program.cs
@@ -0,0 +1,30 @@
+﻿// Task 50 Напишите программу, которая на вход принимает позиции элемента в двумерном массиве 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); 
+            Console.Write($"{matrix[i, j]} \t");
+            
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите m - число строк матрицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n - число столбцов матрицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+InputMatrix(matrix);
+Console.Write("Введите a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+    if (a < m && b < n)
+Console.Write("Значение элемента c указанными позициями: " + matrix[a, b]);
+else 
+Console.Write("Элемента с такими позициями нет в заданном массиве");
---
+++ Task52/Program.cs
@@ -0,0 +1,33 @@
+﻿// Task 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); 
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Average(int[,] matrix)
+{
+   for (int j = 0; j < matrix.GetLength(1); j++)
+    {   double Sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            Sum = Sum + matrix[i, j]; 
+            double Aver = Math.Round(Sum/matrix.GetLength(0), 2);
+            Console.WriteLine($"{Aver}");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine("Cреднее арифметическое каждого столбца:");
+Average(matrix);
